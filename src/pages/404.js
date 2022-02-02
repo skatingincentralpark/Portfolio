f5db9e10819bdbe1ec4fb7abc9feb1d54c8d88bd
charwaii <61100546+charwaii@users.noreply.github.com>
2022-02-02 15:00:29 +1100

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,9 @@
 import * as React from "react";
 import { Container } from "../components/styles/sharedStyles/Container.styled";
-import SEO from "../components/seo/seo";
 
 const NotFoundPage = () => {
   return (
     <>
-      <SEO />
       <Container>Nothing Here</Container>
     </>
   );
